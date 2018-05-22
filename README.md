7c4f86eb771bb82609bd0bfe4946d3e2c2c2614c
florian oswald <florian.oswald@gmail.com>
2018-05-22 21:04:41 +0200

    added examples

--- README.md
+++ README.md
@@ -1,14 +1,26 @@
-# Tasmanian
+# Tasmanian [WIP]
 
 [![Build Status](https://travis-ci.com/floswald/Tasmanian.jl.svg?branch=master)](https://travis-ci.com/floswald/Tasmanian.jl)
 
 wrapping [tasmanian](https://github.com/ORNL/Tasmanian)
 
-## how to
+## Installation
 
-* build the library for a local system? (i.e. travis?)
+To install julia package
 
-```bash
+```julia
+Pkg.clone("git@github.com:floswald/Tasmanian.jl.git")
+```
+
+in terminal, to install the Tasmanian library.
+this will install the library into dir `/Applications/TSG`
+
+
+```
+git clone https://github.com/ORNL/TASMANIAN
+cd TASMANIAN
+mkdir build
+cd build
 cmake \
 -D CMAKE_INSTALL_PREFIX=/Applications/TSG \
 -D Tasmanian_STRICT_OPTIONS=OFF \
@@ -23,4 +35,25 @@ cmake \
 -D Tasmanian_STATIC_LIBRARY=ON \
 -D CMAKE_BUILD_TYPE=Debug \
 ..
+make
+make test
+make install
 ```
+
+### use as python module
+
+see `examples/tastest.py`:
+
+```
+➜  examples git:(master) ✗ python tastest.py 
+TasmanianSG version: 6.0
+TasmanianSG license: BSD 3-Clause with UT-Battelle disclaimer
+
+-------------------------------------------------------------------------------------------------
+Example 1 for OSM: interpolate f(x,y) = cos(0.5 * pi * x) * cos(0.5 * pi * y)
+       using fixed sparse grid with depth 5
+       the error is estimated as the maximum from 1000 random points
+
+ For localp    Number of points: 145   Max. Error: 7.9623370348109734e-03
+```
+
---
+++ examples/tastest.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+
+##############################################################################################################################################################################
+# Copyright (c) 2017, Miroslav Stoyanov
+#
+# This file is part of
+# Toolkit for Adaptive Stochastic Modeling And Non-Intrusive ApproximatioN: TASMANIAN
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+#    or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# UT-BATTELLE, LLC AND THE UNITED STATES GOVERNMENT MAKE NO REPRESENTATIONS AND DISCLAIM ALL WARRANTIES, BOTH EXPRESSED AND IMPLIED.
+# THERE ARE NO EXPRESS OR IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE ANY PATENT,
+# COPYRIGHT, TRADEMARK, OR OTHER PROPRIETARY RIGHTS, OR THAT THE SOFTWARE WILL ACCOMPLISH THE INTENDED RESULTS OR THAT THE SOFTWARE OR ITS USE WILL NOT RESULT IN INJURY OR DAMAGE.
+# THE USER ASSUMES RESPONSIBILITY FOR ALL LIABILITIES, PENALTIES, FINES, CLAIMS, CAUSES OF ACTION, AND COSTS AND EXPENSES, CAUSED BY, RESULTING FROM OR ARISING OUT OF,
+# IN WHOLE OR IN PART THE USE, STORAGE OR DISPOSAL OF THE SOFTWARE.
+##############################################################################################################################################################################
+#
+#
+#  The examples below were adjusted for the OSM 17 lab at BFI Chicago.
+#  Simon Scheidegger, 07/17
+#
+##############################################################################################################################################################################
+
+# necessary import for every use of TASMANIAN
+#
+import sys
+sys.path.append("/Applications/TSG/python")
+import TasmanianSG
+import numpy as np
+
+# imports specifically needed by the examples
+import math
+from random import uniform
+from datetime import datetime
+
+print("TasmanianSG version: {0:s}".format(TasmanianSG.__version__))
+print("TasmanianSG license: {0:s}".format(TasmanianSG.__license__))
+
+grid  = TasmanianSG.TasmanianSparseGrid()
+
+#############################################################################
+
+# EXAMPLE 1 for OSM:
+# interpolate: f(x,y) = cos(0.5 * pi * x) * cos(0.5 * pi * y)
+# using piecewise linear basis functions.
+
+# 1000 2-dimensional sample points 
+aPnts = np.empty([1000, 2])  
+for iI in range(1000):
+    for iJ in range(2):
+        aPnts[iI][iJ] = uniform(-1.0, 1.0)
+
+# Result
+aTres = np.empty([1000,])
+for iI in range(1000):
+    aTres[iI] = math.cos(0.5 * math.pi * aPnts[iI][0]) * math.cos(0.5 * math.pi * aPnts[iI][1])
+
+# Sparse Grid with dimension 2 and 1 output and refinement level 5
+iDim = 2
+iOut = 1
+iDepth = 5
+which_basis = 1 #1= linear basis functions -> Check the manual for other options
+
+print("\n-------------------------------------------------------------------------------------------------")
+print("Example 1 for OSM: interpolate f(x,y) = cos(0.5 * pi * x) * cos(0.5 * pi * y)")
+print("       using fixed sparse grid with depth {0:1d}".format(iDepth))
+print("       the error is estimated as the maximum from 1000 random points\n")
+
+# construct sparse grid
+grid.makeLocalPolynomialGrid(iDim, iOut, iDepth, which_basis, "localp")
+aPoints = grid.getPoints()
+iNumP1 = aPoints.shape[0]
+aVals = np.empty([aPoints.shape[0], 1])
+for iI in range(aPoints.shape[0]):
+    aVals[iI] = math.cos(0.5 * math.pi * aPoints[iI][0]) * math.cos(0.5 * math.pi * aPoints[iI][1])
+grid.loadNeededPoints(aVals)
+
+# compute the error
+aRes = grid.evaluateBatch(aPnts)
+fError1 = max(np.fabs(aRes[:,0] - aTres))
+print(" For localp    Number of points: {0:1d}   Max. Error: {1:1.16e}".format(iNumP1, fError1))
